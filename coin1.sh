b5d343e4fc7a9ed1e4a7ed13da987f4837699a50
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-08-07 06:04:16 +0530

    modified file in usecase1

--- coin1.sh
+++ coin1.sh
@@ -1,1 +1,13 @@
-echo "This is master branch"
+#!/bin/bash -x
+#echo "This is master branch"
+
+isRandom=1;
+val=$(( RANDOM%2 ))
+
+if [[ $val -eq $isRandom ]]
+then
+	echo "Heads"
+else
+	echo "Tails"
+fi
+
