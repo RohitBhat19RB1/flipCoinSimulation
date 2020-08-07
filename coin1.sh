f7c1d5392aabc6cc2d6e20eebffe3e1568ee0758
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-08-07 07:08:41 +0530

    modified file in usecase2

--- coin1.sh
+++ coin1.sh
@@ -1,13 +1,23 @@
 #!/bin/bash -x
 #echo "This is master branch"
 
+p=0;q=0;
 isRandom=1;
-val=$(( RANDOM%2 ))
 
-if [[ $val -eq $isRandom ]]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
 
+echo "giving randomly 6 chances "
+while [[ $(($p + $q)) != 6 ]]
+do
+        val=$(( RANDOM%2 ))
+	if [[ $val -eq $isRandom ]]
+	then
+		echo "Heads"
+		    p=$(($p+1));
+	else
+		echo "Tails"
+		    q=$(($q+1));
+	fi
+done
+
+echo "number of times heads won = $p"
+echo "number of times tails won = $q"
